05f1d009f03bbc070be336f5fcba496c4108e66a
Connor Cook <97connorcook@gmail.com>
2023-06-20 19:19:17 -0700

    Using react-big-calendar for schedule -A

--- frontend/src/components/create-schedule.js
+++ frontend/src/components/create-schedule.js
@@ -1,30 +1,22 @@
+import '../css/create-schedule.css'
 
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+
+const localizer = momentLocalizer(moment)
 
 function CreateSchedule() {
-  const generateTimeSlots = (startTime, endTime, intervalMinutes) => {
-    const timeSlots = [];
-    let currentTime = new Date(startTime);
-
-    while (currentTime <= endTime) {
-      const formattedTime = currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-      timeSlots.push(formattedTime);
-
-      currentTime.setMinutes(currentTime.getMinutes() + intervalMinutes);
-    }
-
-    return timeSlots;
-  };
-
-  const startTime = new Date();
-  startTime.setHours(6, 0, 0); // Start time at 6:00 AM
-  const endTime = new Date();
-  endTime.setHours(20, 0, 0); // End time at 8:00 PM
-
-  const timeSlots = generateTimeSlots(startTime, endTime, 15);
+  
   return (
-    <div>{timeSlots.map((time) => (
-      <li>{time}</li>
-    ))}</div>
+    <div>
+    <Calendar
+      localizer={localizer}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
+    
   )
 }
 
