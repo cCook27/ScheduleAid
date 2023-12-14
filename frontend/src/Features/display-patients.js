5a25f9eb84838abd40ee3b19c5a4d248991eae70
Connor Cook <97connorcook@gmail.com>
2023-12-13 22:25:27 -0700

    Redoing Edit parameters

--- frontend/src/Features/display-patients.js
+++ frontend/src/Features/display-patients.js
@@ -37,7 +37,13 @@ const DisplayPatients = ({ handleDragStart, homes, homeStatus, myEvents, start, 
  
 
   return (
-    <div className="row patient-display">
+    <div className="container">
+      <div classparam="row py-2">
+        <div classparam="col d-flex justify-content-center">
+          <h4 classparam="title">Patients</h4>
+        </div>
+      </div>
+      <div className="row patient-display">
       {homeStatus === 'loading' ? (
         <div><Loading /></div>
       ) : homeStatus === 'error' ? (
@@ -74,8 +80,10 @@ const DisplayPatients = ({ handleDragStart, homes, homeStatus, myEvents, start, 
               )
             )
           )
-      }
+        }
+      </div>
     </div>
+    
   )
 }
 
