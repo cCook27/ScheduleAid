a18c9dce9431415ba49341bbfa79fe9b1b18672d
Connor Cook <97connorcook@gmail.com>
2023-12-14 11:54:24 -0700

    Group View styling

--- frontend/src/Features/display-groups.js
+++ frontend/src/Features/display-groups.js
@@ -6,8 +6,6 @@ import "../css/display-groups.css"
 const DisplayGroups = ({ handleDragStart, homes, patientGroups, doubleSessions, myEvents, start, end, handleEventsUpdate, handleUpdatedGroups }) => {
 
   const [groups, setGroups] = useState([]);
-  // const [groupingManual, setGroupingManual] = useState([]);
-  
  
   useEffect(() => {
     if(patientGroups) {
@@ -80,95 +78,70 @@ const DisplayGroups = ({ handleDragStart, homes, patientGroups, doubleSessions, 
     }
   };
 
+  
   return (
     <div className="container">
-      <div className="row">
+      <div className="row d-flex justify-content-center align-items-center">
         {!homes ? 
-        (
-          <div className="col">
-            <div>No Patients saved</div>     
-          </div>
+          (
+            <div className="col">
+              <div>No Patients saved</div>     
+            </div>
           ) : 
           
           !patientGroups ? 
-          (
-            <div><Loading /></div>
-          ) : 
+            (
+              <div><Loading /></div>
+            ) : 
           
-          (groups.length >= 1 && groups.length <= 7) ? 
+          groups.length >= 1 && groups.length <= 7 ? 
           
-          (
-            groups.map((group, index) => (
-              <div className="col-6 group">
-                <div className="row">
+            (
+              groups.map((group, index) => (
+                <div className="col-6 d-flex flex-column group">
+                  <div className="group-number">Group {index + 1}</div>
+                  <div className="d-flex justify-content-center align-items-center row">
 
-                  {group.map((patient) => (
+                {
+                  group.map((patient) => (
                     patient.scheduled === true ? 
+
                     (
-                      <div className="col-12"> 
-                        <div key={patient._id} className="used">{patient.name}</div>
+                      <div className="col-6 d-flex patient-cont used" key={patient._id} >
+                        <div className="patient-name ellipsis-overflow">
+                          {patient.name}
+                        </div>
                       </div>
-                    ) : 
-                    
+                    ) :
+
                     (
-                      <div key={patient._id} draggable onDragStart={() => handleDragStart(patient.name, patient.address, patient.coordinates, index)} className="col-12">
-                        {patient.name}
+                      <div className="col-6 d-flex patient-cont" key={patient._id} draggable onDragStart={() => handleDragStart(patient.name, patient.address, patient.coordinates, index)}>
+                        <div className="patient-name ellipsis-overflow">
+                          {patient.name}
+                        </div>
                       </div>
                     )
-                  ))}
 
-                </div>
-              </div>
-            ))
-          ) : 
-          
-          (
-            patientGroups.map((patient) => (
-              <div className="col-6 group">
-                <div className="row">
-                  <div draggable onDragStart={() => handleDragStart(patient.name, patient.address, patient.coordinates)} className="col-12">
-                    {patient.name}
+                  ))
+                }
+
                   </div>
                 </div>
-              </div>
-            ))
-          )
+              ))
+            ) :
+
+            (
+              <div>contrary</div>
+            )
         }
       </div>
-
-      {doubleSessions && (
-        <div className="row">
-          {doubleSessions.map((session) => (
-            <div className="col-4">{session.name}</div>
-          ))}
-        </div>
-      )}
-
     </div>
-   
+  
   )
 }
 
 export default DisplayGroups;
 
-// if(isScheduled.length > 0) {
-//   const hasGroupNum = isScheduled.filter((ev) => ev.groupNumber === index);
-//   const noGroupNum = isScheduled.filter((ev) => ev.groupNumber !== index);
 
-//   if(hasGroupNum.length === frequency - 1 && noGroupNum.length === 1) {
-//     let schedEvent = noGroupNum[0];
-//     schedEvent.groupNumber = index;
-//     updateEvents(schedEvent);
-//     return patient;
-//   };
 
-//   if(hasGroupNum.length === 0) {
-//     patient.scheduled = false;
-//     return patient;
-//   };
 
-//   if(hasGroupNum.length === 1) {
-//     patient.scheduled = true;
-//     return patient;
-//   };
-// };
